8465db1bd6b368dc550dc1d482411027b0966965
pallab nath <you@example.com>
2023-04-15 12:23:56 +0530

    donee

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -1,27 +1,93 @@
 package com.driver;
 
-
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Repository
 public class StudentRepository {
-    Map <String,Student> studentDB = new HashMap<>();
+    HashMap<String,Student> studentDb = new HashMap<>();
+    HashMap<String,Teacher> teacherDb = new HashMap<>();
+    HashMap<Teacher,List<Student>> studentTeacher = new HashMap<>();
+
 
     public void addStudent(Student student)
     {
-        String key = student.getName();
-        studentDB.put(key,student);
+        studentDb.put(student.getName(), student);
     }
 
-    public Student getStudentByName(String name)
+    public void addTeacher(Teacher teacher)
     {
-        if(studentDB.containsKey(name))
+        teacherDb.put(teacher.getName(), teacher);
+    }
+    public void addStudentTeacherPair(String studentName, String teacherName)
+    {
+        Teacher teacher = teacherDb.get(teacherName);
+        Student student = studentDb.get(studentName);
+        if(studentTeacher.containsKey(teacher))
         {
-            return studentDB.get(name);
+            List<Student> students = studentTeacher.get(teacher);
+            students.add(student);
+            studentTeacher.put(teacher,students);
         }
-        return null;
+        else
+        {
+            List<Student> students = new ArrayList<>();
+            students.add(student);
+            studentTeacher.put(teacher,students);
+        }
+    }
+
+    public List<Student> getAllStudents()
+    {
+        List<Student> students = new ArrayList<>();
+        for(Student student: studentDb.values())
+        {
+            students.add(student);
+        }
+        return students;
+    }
+
+    public void deleteTeacherByName(String name)
+    {
+        Teacher teacher = teacherDb.get(name);
+        teacherDb.remove(name);
+        for(Student student: studentTeacher.get(teacher))
+        {
+            studentDb.remove(student.getName());
+        }
+        studentTeacher.remove(teacher);
+    }
+
+    public void deleteAllTeachers()
+    {
+        for(Teacher teacher: teacherDb.values())
+        {
+            for(Student student: studentTeacher.get(teacher))
+            {
+                studentDb.remove(student.getName());
+            }
+        }
+        studentTeacher.clear();
+        teacherDb.clear();
+    }
+
+    public List<Teacher> getAllTeachers()
+    {
+        List<Teacher> teachers = new ArrayList<>();
+        for(Teacher teacher: teacherDb.values())
+        {
+            teachers.add(teacher);
+        }
+        return teachers;
+    }
+
+    public List<Student> getStudentByTeacherName(String teacherName)
+    {
+        if(studentTeacher.containsKey(teacherDb.get(teacherName)))
+            return studentTeacher.get(teacherDb.get(teacherName));
+        return new ArrayList<>();
     }
 }
